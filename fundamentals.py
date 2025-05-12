c0fe3a7710aa068f44c3bd5e07aef4ae3e2955e9
fahim-ysr <fyasir@mun.ca>
2025-05-12 14:52:00 -0230

    Completed Phase 1: Setting up Multimodal LLM | Added sample dandruff image

---
+++ fundamentals.py
@@ -0,0 +1,52 @@
+# Importing modules
+import base64
+from groq import Groq
+
+
+# !Setting up GROQ API Key
+
+import os                                                                          
+from dotenv import load_dotenv
+from pathlib import Path
+load_dotenv(Path(".env.local"))
+KEY = os.getenv("GROQ_API_KEY")
+
+
+# !Converting image to required format
+image_path = "sample_dandruff.jpg"
+image_file = open(image_path, "rb")
+encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+
+
+# !Setting up Multimodal LLM
+
+client = Groq(api_key= KEY)
+current_model = "meta-llama/llama-4-scout-17b-16e-instruct"
+query = "Is there something wrong with my face?"
+
+# Setting up API call to Groq
+message = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "text",
+                "text": query
+            },
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:image/jpeg;base64,{encoded_image}",
+                },
+            },
+        ],
+    }]
+
+chat_complete = client.chat.completions.create(
+    messages = message,
+    model = current_model
+)
+
+temp_output = chat_complete.choices[0].message
+string_content = temp_output.content
+print(string_content)
