efb2eabb8637fb9b0511488a5cb1cbd16f17c169
fahim-ysr <fyasir@mun.ca>
2025-05-18 17:04:26 -0230

    Phase 1: Updated | Converterd operations into functions for more flexibility

--- fundamentals.py
+++ fundamentals.py
@@ -14,40 +14,54 @@ KEY = os.getenv("GROQ_API_KEY")
 
 # !Converting image to required format
 
-image_path = "sample_dandruff.jpg"
-image_file = open(image_path, "rb")
-encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+# This function converts image to required format
+def image_encode(image_path):
+    image_file = open(image_path, "rb")
+    encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+    return encoded_image
 
+image_path = "sample_dandruff.jpg"
+encoded_image = image_encode(image_path)
 
 # !Setting up Multimodal LLM to analyze image with text
 
-client = Groq(api_key= KEY)
+def analyze_image_and_query(encoded_image, query, model):
+    client = Groq(api_key= KEY)
+    
+
+    # Setting up API call to Groq
+    message = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": query
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{encoded_image}",
+                    },
+                },
+            ],
+        }]
+
+    chat_complete = client.chat.completions.create(
+        messages = message,
+        model = current_model
+    )
+
+    # Formatting output to extract response
+    temp_output = chat_complete.choices[0].message
+    string_content = temp_output.content
+    
+    # Returns the response
+    return string_content
+
+
+# Testing the model
+
 current_model = "meta-llama/llama-4-scout-17b-16e-instruct"
 query = "Is there something wrong with my face?"
-
-# Setting up API call to Groq
-message = [
-    {
-        "role": "user",
-        "content": [
-            {
-                "type": "text",
-                "text": query
-            },
-            {
-                "type": "image_url",
-                "image_url": {
-                    "url": f"data:image/jpeg;base64,{encoded_image}",
-                },
-            },
-        ],
-    }]
-
-chat_complete = client.chat.completions.create(
-    messages = message,
-    model = current_model
-)
-
-temp_output = chat_complete.choices[0].message
-string_content = temp_output.content
-print(string_content)
+print(analyze_image_and_query(encoded_image= encoded_image, query= query, model= current_model))
